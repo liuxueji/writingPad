0dd9f4f91cf3590afa301fb6c9898519e2a24bf2
xxxCxx <1290746759@qq.com>
2022-06-30 12:00:11 +0800

    add 手写板V2.0

---
+++ writingPad_V2/entry/src/main/java/com/example/writingpad_v2/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.writingpad_v2;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
